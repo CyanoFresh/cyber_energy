0e0b729d21c55f9cc983af1572aa9752c5aa3711
CyanoFresh <cyanofresh@gmail.com>
2020-10-10 01:03:59 +0300

    added test fetch

--- src/SelectForm.js
+++ src/SelectForm.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import {
-  MuiPickersUtilsProvider,
   KeyboardDatePicker,
+  MuiPickersUtilsProvider,
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { Button, CardContent } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import makeStyles from '@material-ui/core/styles/makeStyles';
+import { API_KEY, API_URL, sheetId } from './config';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,6 +22,16 @@ export function SelectForm() {
   const [selectedDate, setSelectedDate] = React.useState(
     new Date('2014-08-18T21:11:54')
   );
+
+  useEffect(() => {
+    const load = async () => {
+      await fetch(
+        `${API_URL}/${sheetId}/values:batchGet?key=${API_KEY}&ranges=725033TY.csv!A3:C&ranges=725033TY.csv!AF3:AF&ranges=725033TY.csv!AR3:AR&ranges=725033TY.csv!AU3:AU`
+      );
+    };
+
+    load();
+  }, []);
 
   const handleDateChange = date => {
     setSelectedDate(date);
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const API_URL = 'https://sheets.googleapis.com/v4/spreadsheets';
+export const API_KEY = 'AIzaSyBIGs6RRL6WtWnCZpOZwjIHaSdO8vfVyBk';
+export const sheetId = '1qcC9ww0yufzxoGVCGE41XsIQ8N3oEw4ZG7lzrALzh0Q';
