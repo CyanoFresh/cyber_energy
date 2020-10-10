c7882ae508009746ceda22c43d56e98741f1aa66
annpinchuk <62667436+annpinchuk@users.noreply.github.com>
2020-10-11 00:03:42 +0300

    added getDirection()

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,2 +1,26 @@
 export const getSheetIdFromUrl = url =>
   url.split('/').find((part, index, parts) => parts[index - 1] === 'd');
+
+const directions = {
+  N: 0,
+  NE: 45,
+  E: 90,
+  SE: 135,
+  S: 180,
+  SW: 225,
+  W: 270,
+  NW: 315,
+};
+
+export const getDirection = degree => {
+  degree %= 360;
+
+  for (let [direction, directionDegree] of Object.entries(directions)) {
+    if (directionDegree < 22.5) {
+      directionDegree += 22.5;
+    }
+    if (degree >= directionDegree - 22.5 && degree < directionDegree + 22.5) {
+      return direction;
+    }
+  }
+};
