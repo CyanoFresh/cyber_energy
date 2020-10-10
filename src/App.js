7f14e105f1c699b02590e839e3380584ae0a4c59
CyanoFresh <cyanofresh@gmail.com>
2020-10-10 17:33:45 +0300

    decomposed App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import React from "react";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { ThemeProvider } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Paper from "@material-ui/core/Paper";
-import Container from "@material-ui/core/Container";
-import { TopMenu } from "./TopMenu";
-import { theme } from "./theme";
-import { SelectForm } from "./SelectForm";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import { Tab1 } from "./Tab1";
+import React from 'react';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { TopMenu } from './TopMenu';
+import { theme } from './theme';
+import { SelectForm } from './SelectForm';
+import { TabPanels } from './TabPanels';
 
 const useStyles = makeStyles(theme => ({
-  toolbar: theme.mixins.toolbar
+  toolbar: theme.mixins.toolbar,
 }));
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
 
 function App() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => setValue(newValue);
 
   return (
     <ThemeProvider theme={theme}>
@@ -54,27 +26,8 @@ function App() {
 
         <Container maxWidth="md">
           <SelectForm />
-          <Paper className={classes.root}>
-            <Tabs centered value={value} onChange={handleChange}>
-              <Tab label="Вкладка 1" />
-              <Tab label="Вкладка 2" />
-              <Tab label="Вкладка 3" />
-              <Tab label="Вкладка 4" />
-            </Tabs>
 
-            <TabPanel value={value} index={0}>
-              <Tab1 />
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              Item Two
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              Item Three
-            </TabPanel><TabPanel value={value} index={3}>
-            Item Four
-          </TabPanel>
-
-          </Paper>
+          <TabPanels />
         </Container>
       </main>
     </ThemeProvider>
---
+++ src/TabPanels.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { Tab1 } from './Tab1';
+import Box from '@material-ui/core/Box';
+
+function TabPanel({ children, value, index, ...other }) {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={2}>{children}</Box>}
+    </div>
+  );
+}
+
+export function TabPanels() {
+  const [value, setValue] = useState(0);
+
+  const onChange = (event, newValue) => setValue(newValue);
+
+  return (
+    <Paper>
+      <Tabs centered value={value} onChange={onChange}>
+        <Tab label="Вкладка 1" />
+        <Tab label="Вкладка 2" />
+        <Tab label="Вкладка 3" />
+        <Tab label="Вкладка 4" />
+      </Tabs>
+
+      <TabPanel value={value} index={0}>
+        <Tab1 />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Tab Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Tab Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Tab Four
+      </TabPanel>
+    </Paper>
+  );
+}
